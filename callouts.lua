0b1830ea7f0e2bc053d59b5258b74852d7f4a4ec
Nia Hughes <support@bioinformatics.ca>
2025-08-11 14:07:19 -0400

    lua update

--- callouts.lua
+++ callouts.lua
@@ -19,36 +19,25 @@ local function create_icon_element(icon_attr, type)
 
   if show_icon then
     local icon_fa_class = (icon_attr and icon_attr ~= "true") and icon_attr or get_default_icon(type)
-    local i_tag = pandoc.RawInline('html', '<i class="' .. icon_fa_class .. '" aria-hidden="true"></i>')
-    return pandoc.Span({i_tag}, {class = "callout-icon"})
+    -- Return the raw HTML for the icon
+    return '<i class="' .. icon_fa_class .. '" aria-hidden="true"></i>'
   end
-  return nil
+  return ''
 end
 
-local function create_title_element(title_text, center_title_attr, is_summary_title, summary_title_class, static_title_class_arg)
-  if title_text and title_text ~= "" then
-    local title_inline_content
-    local parsed_title_blocks = pandoc.read(title_text, 'markdown').blocks
-    if parsed_title_blocks and #parsed_title_blocks > 0 and parsed_title_blocks[1].t == "Para" then
-        title_inline_content = parsed_title_blocks[1].content
-    else
-        title_inline_content = {pandoc.Str(title_text)}
+local function create_title_element_str(title_text)
+    if title_text and title_text ~= "" then
+        local parsed_title_blocks = pandoc.read(title_text, 'markdown').blocks
+        if parsed_title_blocks and #parsed_title_blocks > 0 then
+            -- Use Pandoc to convert the title markdown to an HTML string
+            return pandoc.write(pandoc.Pandoc(parsed_title_blocks), 'html'):gsub("^<p>", ""):gsub("</p>\n?$", "")
+        else
+            return pandoc.utils.stringify(title_text)
+        end
     end
-
-    if is_summary_title then
-        return pandoc.Span(title_inline_content, {class = summary_title_class or "callout-dropdown-title"})
-    else
-        local title_block_content = pandoc.Para(title_inline_content)
-        local current_static_title_class = static_title_class_arg or "callout-title-static"
-        local title_classes_list = {current_static_title_class}
-        if center_title_attr == "true" then table.insert(title_classes_list, "center") end
-        local kv_attributes_list = { {"style", "margin-bottom: 0.5em;"} }
-        local final_attrs = pandoc.Attr("", title_classes_list, kv_attributes_list)
-        return pandoc.Div({title_block_content}, final_attrs)
-    end
-  end
-  return nil
+    return ''
 end
+
 
 function Div(el)
   if el.classes:includes("callout") then
@@ -64,15 +53,14 @@ function Div(el)
       return el
     end
 
-    local icon_element = create_icon_element(icon_attr, type)
-
     if is_collapsible then
+      -- For collapsible callouts, we build the entire HTML as a single string to ensure correct nesting.
       local details_classes = {"callout-dropdown"}
-      local base_type_class = "callout-" .. type -- e.g. callout-red, for icon color on plain style
+      local base_type_class = "callout-" .. type
 
       if style == "plain" then
         table.insert(details_classes, "callout-plain")
-        table.insert(details_classes, base_type_class) -- Add type class for potential icon coloring
+        table.insert(details_classes, base_type_class)
       elseif style == "subtle" then
         table.insert(details_classes, "callout-subtle")
         table.insert(details_classes, base_type_class .. "-subtle")
@@ -81,46 +69,68 @@ function Div(el)
       end
 
       local summary_classes = {"callout-dropdown-summary"}
-      if icon_element and style == "plain" then -- Add helper class if icon present in plain summary
+      local icon_html = create_icon_element(icon_attr, type)
+      if icon_html ~= '' and style == "plain" then
         table.insert(summary_classes, "summary-has-icon")
       end
 
-      local summary_children = {}
-      if icon_element then table.insert(summary_children, icon_element) end
-
       local summary_title_text_val = title_text or "Details"
-      local summary_title_element = create_title_element(summary_title_text_val, nil, true, "callout-dropdown-title", nil)
-      if summary_title_element then table.insert(summary_children, summary_title_element) end
+      local title_html = create_title_element_str(summary_title_text_val)
 
-      local caret_i_tag = pandoc.RawInline('html', '<i class="fas fa-chevron-right" aria-hidden="true"></i>')
-      local caret_span_element = pandoc.Span({caret_i_tag}, {class = "callout-dropdown-caret"})
-      table.insert(summary_children, caret_span_element)
+      local caret_html = '<span class="callout-dropdown-caret"><i class="fas fa-chevron-right" aria-hidden="true"></i></span>'
 
-      local plain_block_for_summary = pandoc.Plain(summary_children)
-      local temp_doc_for_summary = pandoc.Pandoc({plain_block_for_summary})
-      local summary_html_content = pandoc.write(temp_doc_for_summary, 'html')
-      summary_html_content = summary_html_content:gsub("^<p>", ""):gsub("</p>\n?$", "")
+      -- Combine all parts of the summary, adding a non-breaking space after the icon.
+      local summary_content_html = icon_html .. '&nbsp;&nbsp;&nbsp;<span class="callout-dropdown-title">' .. title_html .. '</span> ' .. caret_html
+      local summary_html = '<summary class="'.. table.concat(summary_classes, " ") ..'">' .. summary_content_html .. '</summary>'
 
-      local summary_element_html = '<summary class="'.. table.concat(summary_classes, " ") ..'">' .. summary_html_content .. '</summary>'
-      local content_div_element = pandoc.Div(el.content, {class = "callout-dropdown-content"})
-      local details_open_tag = '<details class="' .. table.concat(details_classes, " ") .. '">'
-      local details_close_tag = '</details>'
+      -- Convert the div's content to HTML
+      local body_content_html = pandoc.write(pandoc.Pandoc(el.content), 'html')
+      local body_html = '<div class="callout-dropdown-content">' .. body_content_html .. '</div>'
 
-      return {
-          pandoc.RawBlock('html', details_open_tag),
-          pandoc.RawBlock('html', summary_element_html),
-          content_div_element,
-          pandoc.RawBlock('html', details_close_tag)
-      }
-    else -- Static callout
+      -- Assemble the final, complete <details> block
+      local final_html = '<details class="' .. table.concat(details_classes, " ") .. '">' .. summary_html .. body_html .. '</details>'
+
+      return pandoc.RawBlock('html', final_html)
+
+    else -- Static callout (your original logic for this was sound)
       local specific_style_class = "callout-" .. type
       if style == "important" then specific_style_class = specific_style_class .. "-important"
       elseif style == "subtle" then specific_style_class = specific_style_class .. "-subtle"
       end
-      -- Note: style="plain" for static callouts is not specifically handled here to remove borders/bg,
-      -- users would need a .callout-plain static CSS if desired. Focus is on plain dropdown.
 
-      local title_element = create_title_element(title_text, center_title, false, nil, "callout-title-static")
+      -- create_title_element and create_icon_element need to return pandoc elements for the static case
+      -- Re-defining local helper functions for the static case to return pandoc objects
+      local function create_icon_element_static(icon_attr, type)
+        local show_icon = true
+        if icon_attr == "false" then show_icon = false end
+        if show_icon then
+          local icon_fa_class = (icon_attr and icon_attr ~= "true") and icon_attr or get_default_icon(type)
+          local i_tag = pandoc.RawInline('html', '<i class="' .. icon_fa_class .. '" aria-hidden="true"></i>')
+          return pandoc.Span({i_tag}, {class = "callout-icon"})
+        end
+        return nil
+      end
+
+      local function create_title_element_static(title_text, center_title_attr)
+        if title_text and title_text ~= "" then
+          local title_inline_content
+          local parsed_title_blocks = pandoc.read(title_text, 'markdown').blocks
+          if parsed_title_blocks and #parsed_title_blocks > 0 and parsed_title_blocks[1].t == "Para" then
+              title_inline_content = parsed_title_blocks[1].content
+          else
+              title_inline_content = {pandoc.Str(title_text)}
+          end
+          local title_block_content = pandoc.Para(title_inline_content)
+          local title_classes_list = {"callout-title-static"}
+          if center_title_attr == "true" then table.insert(title_classes_list, "center") end
+          return pandoc.Div({title_block_content}, {class=table.concat(title_classes_list, " ")})
+        end
+        return nil
+      end
+
+      local icon_element = create_icon_element_static(icon_attr, type)
+      local title_element = create_title_element_static(title_text, center_title)
+
       local content_div_inner_blocks = {}
       if title_element then table.insert(content_div_inner_blocks, title_element) end
       for _, block in ipairs(el.content) do table.insert(content_div_inner_blocks, block) end
@@ -136,4 +146,5 @@ function Div(el)
   return nil
 end
 
-return {{Div = Div}}
+-- A filter must return a list of filter definitions.
+return { {Div = Div} }
